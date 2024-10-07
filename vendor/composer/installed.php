d8ad16e748dc9f8a9d90d59377e5565fb362b122
hamilcaron <hamilcaronolan@gmail.com>
2024-10-07 12:18:16 +0200

    commit Q15

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/cafe',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6730ce219984a040a90bcbe3b0eb6e1f927e17f7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/cafe' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6730ce219984a040a90bcbe3b0eb6e1f927e17f7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
