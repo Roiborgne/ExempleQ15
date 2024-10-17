a071c933c2a0cf9e52bf9c2c8d7f711af46b95e1
NH <146842064+Roiborgne@users.noreply.github.com>
2024-10-17 11:00:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
+include_once "vendor/autoload.php";
 
-if(isset($_REQUEST["case"]))
+use App\Utilitaire\Vue;
+use App\Vue\Vue_BienvenueAllerPagePrecedente;
+use App\Vue\Vue_BienvenueAllerPageSuivante;
+
+if (isset($_REQUEST["case"]))
     $case = $_REQUEST["case"];
 else
     $case = "defaut";
 
+if (isset($_REQUEST["action"]))
+    $action = $_REQUEST["action"];
+else
+    $action = "defaut";
 
+$Vue = new Vue();
 switch ($case) {
+    case "A":
     case "defaut":
-        include("src/Vue/Vue_AccueilCaseA.php");
+        include ".\src\Controleur\caseA.php";
+        break;
+    case "B":
+        include ".\src\Controleur\caseB.php";
+        break;
+    case "IMC":
+        include ".\src\Controleur\calculeIMC.php";
+        break;
+    case "Orage":
+        include ".\src\Controleur\calculeOrage.php";
         break;
 }
+$Vue->afficher();
